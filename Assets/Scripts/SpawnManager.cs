937cee1a34caf316829830cb5b32897b48dc65f4
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2026-07-25 23:49:10 +0300

    Refactor SpawnManager: Improve enemy spawning logic and enhance spawn position validation

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,27 +3,47 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float minSpawnX = -23f;
+    [SerializeField] private float maxSpawnX = 23f;
+    [SerializeField] private float minSpawnZ = 0f;
+    [SerializeField] private float maxSpawnZ = 23f;
+
+    [SerializeField] private float spawnCheckRadius = 0.75f;
+    [SerializeField] private int maxSpawnAttempts = 20;
+    [SerializeField] private int spawnAmount = 5;
+    [SerializeField] private LayerMask characterLayer;
+
     void Start()
     {
-        InvokeRepeating(nameof(SpawnEnemy), 1f, 5f);
+        InvokeRepeating(nameof(SpawnEnemyWave), 1f, 5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnEnemyWave()
     {
-
+        for (int spawn = 0; spawn < spawnAmount; spawn++)
+        {
+            SpawnEnemy();
+        }
     }
 
     void SpawnEnemy()
     {
 
-        for (int i = 0; i < 5; i++)
+        for (int attempt = 0; attempt < maxSpawnAttempts; attempt++)
         {
-            int posX = Random.Range(-23, 23);
-            int posZ = Random.Range(0, 23);
+            float randomX = Random.Range(minSpawnX, maxSpawnX);
+            float randomZ = Random.Range(minSpawnZ, maxSpawnZ);
 
-            Instantiate(enemyPrefab, new Vector3(posX, 0.5f, posZ), transform.rotation);
+            Vector3 spawnPosition = new(randomX, 0.5f, randomZ);
+
+            //Check the spawnposition for an existing character and try again if found
+            bool positionBlocked = Physics.CheckSphere(spawnPosition, spawnCheckRadius, characterLayer);
+
+            if (!positionBlocked)
+            {
+                Instantiate(enemyPrefab, spawnPosition, transform.rotation);
+                return;
+            }
         }
     }
 }
