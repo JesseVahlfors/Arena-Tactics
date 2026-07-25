7b455b44c8582da5556cfe941f81089cc2e51f9c
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2026-07-25 22:56:23 +0300

    Add enemy spawning functionality and create enemy prefab
    - Implemented enemy spawning in SpawnManager with a repeating invoke method.
    - Created a new Enemy prefab with necessary components including Transform, MeshFilter, MeshRenderer, BoxCollider, Rigidbody, and a custom AIController script.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private GameObject enemyPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating(nameof(SpawnEnemy), 1f, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnEnemy()
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            int posX = Random.Range(-23, 23);
+            int posZ = Random.Range(0, 23);
+
+            Instantiate(enemyPrefab, new Vector3(posX, 0.5f, posZ), transform.rotation);
+        }
     }
 }
